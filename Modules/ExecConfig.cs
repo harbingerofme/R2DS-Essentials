731d5aa15f6df66a47afb151805d7583f65ecc1c
Harbingerofme <harbingerofme@gmail.com>
2019-12-23 15:52:44 +0100

    Update ExecConfig to wait until all plugins are loaded.

--- Modules/ExecConfig.cs
+++ Modules/ExecConfig.cs
@@ -1,37 +1,45 @@
 ﻿using BepInEx.Configuration;
 using RoR2;
+using System;
+using Console = RoR2.Console;
 
 namespace R2DSEssentials.Modules
 {
-    [Module(ModuleName, ModuleDescription, DefaultEnabled, ModuleAttribute.StartupTarget.Start)]
+    [Module(ModuleName, ModuleDescription, DefaultEnabled, ModuleAttribute.StartupTarget.Awake)]
     internal sealed class ExecConfig : R2DSEModule
     {
         public const string ModuleName = nameof(ExecConfig);
-        public const string ModuleDescription = "Execute the server config file of your choice located in Risk of Rain 2_Data/Config/ (server.cfg by default).";
+        public const string ModuleDescription = "(Re) Execute the server config file of your choice located in Risk of Rain 2_Data/Config/ (server.cfg by default).";
         public const bool   DefaultEnabled = true;
 
         private ConfigEntry<string> _configFileName;
 
         public ExecConfig(string name, string description, bool defaultEnabled) : base(name, description, defaultEnabled)
         {
-            if(IsEnabled)
-                Console.instance.SubmitCmd(null, $"exec {_configFileName.Value}");
         }
 
 
         protected override void Hook()
         {
-
+            PluginEntry.Instance.OnFinishLoading += Go;
         }
 
         protected override void UnHook()
         {
-
+            PluginEntry.Instance.OnFinishLoading -= Go;
         }
 
         protected override void MakeConfig()
         {
             _configFileName = AddConfig("Server CFG Filename", "server", "Name of the CFG File to load at the startup of the server. Don't include the extension. Example : server");
+        }
+
+        internal void Go(object _, EventArgs __)
+        {
+            if (IsEnabled)
+                Console.instance.SubmitCmd(null, $"exec {_configFileName.Value}");
+            else
+                Logger.LogWarning("ExecConfig: Go() called while not enabled!");
         }
     }
 }
