1a222991fc88580a7cc4f5a42937a1946f649bcf
Harbingerofme <harbingerofme@gmail.com>
2019-12-23 18:15:45 +0100

    giving up for now.

--- Modules/ChatCommands.cs
+++ Modules/ChatCommands.cs
@@ -1,8 +1,16 @@
 ﻿
 
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using static MonoMod.Cil.RuntimeILReferenceBag.FastDelegateInvokers;
+
 namespace R2DSEssentials.Modules
 {
-    [Module(ModuleName, ModuleDescription, DefaultEnabled)]
+
+    //Disabled!
+
+
+    //[Module(ModuleName, ModuleDescription, DefaultEnabled)]
     class ChatCommands : R2DSEModule
     {
         public const string ModuleName = nameof(ChatCommands);
@@ -15,6 +23,37 @@ namespace R2DSEssentials.Modules
 
         protected override void Hook()
         {
+            IL.RoR2.Chat.CCSay += Chat_CCSay;
+        }
+
+        private void Chat_CCSay(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+            c.GotoNext(x => x.MatchLdarga(out _));
+            c.Emit(OpCodes.Ldarga_S,0);
+            c.EmitDelegate<Func<RoR2.ConCommandArgs,bool>>((args) =>
+            {
+                if (args[0].StartsWith("/"))
+                {
+                    string command = args[0].Substring(1);
+                    Logger.LogMessage("HAI!");
+                    RoR2.Console.instance.SubmitCmd(args.sender, command);
+                    return true;
+                }
+                return false;
+            });
+            c.Emit(OpCodes.Brtrue,19);
+            /*
+            if (args[0].StartsWith("/"))
+            {
+                string command = args[0].Substring(1);
+                Logger.LogInfo($"Intercepted: {command}");
+                RoR2.Console.instance.SubmitCmd(args.sender, command);
+            }
+            else
+            {
+                orig(args);
+            }*/
         }
 
         protected override void MakeConfig()
@@ -24,6 +63,7 @@ namespace R2DSEssentials.Modules
 
         protected override void UnHook()
         {
+            IL.RoR2.Chat.CCSay -= Chat_CCSay;
         }
     }
 }
