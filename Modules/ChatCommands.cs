99d4fa9d5f4e2e3ccd97ddfd199f2d67f355d11b
Harbingerofme <harbingerofme@gmail.com>
2019-12-23 16:13:37 +0100

    Create ChatCommands.cs

---
+++ Modules/ChatCommands.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace R2DSEssentials.Modules
+{
+    [Module(ModuleName, ModuleDescription, DefaultEnabled)]
+    class ChatCommands : R2DSEModule
+    {
+        public const string ModuleName = nameof(ChatCommands);
+        public const string ModuleDescription = "Capture messages beginning with '/' and consider them as though a user send them as a command. WIP";
+        public const bool DefaultEnabled = false;
+
+        public ChatCommands(string name, string description, bool defaultEnabled) : base(name, description, defaultEnabled)
+        {
+        }
+
+        protected override void Hook()
+        {
+        }
+
+        protected override void MakeConfig()
+        {
+           //can be empty.
+        }
+
+        protected override void UnHook()
+        {
+        }
+    }
+}
