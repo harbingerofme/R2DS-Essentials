5e2b89224221ea396fbbf046b16a50c4b90f8bc0
Harbingerofme <harbingerofme@gmail.com>
2020-01-02 23:58:32 +0100

    Create Console.cs

---
+++ Util/Console.cs
@@ -0,0 +1,21 @@
+﻿using RoR2;
+
+namespace R2DSEssentials.Util
+{
+    static class Console
+    {
+        public static string MergeArgs(ConCommandArgs args,int fromIndex)
+        {
+            if (fromIndex > args.Count)
+                return "";
+            if (fromIndex < 0)
+                fromIndex = 0;
+            string[] strArgs = new string[args.Count - fromIndex];
+            for(int i = fromIndex; i < args.Count; i++)
+            {
+                strArgs[i - fromIndex] = args[i];
+            }
+            return string.Join(" ", strArgs);
+        }
+    }
+}
